2e4573483de5c663e3789d52fb6b1713c8e1076c
owenjonesuob <olj23@bath.ac.uk>
2017-04-15 14:40:02 +0100

    Correct some nonsense

--- flow.R
+++ flow.R
@@ -1,16 +1,18 @@
-# Load required library=ies
+# Load required libraries
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(gridExtra)
 
+load("imdb.rda")
 # Remove duplicates from imdb: resulting data frame is called imdc
 source("dups.R")
 
 # Have a look at the data: see exploration.R
 
-# Remove outliers which become obvious from plots
-imdc <- filter(imdc, aspect < 10, budget < 3.5e+08)
+# Fix outliers which become obvious from plots
+imdc$aspect[imdc$aspect == 16] <- 1.78
+imdc$budget[imdc$budget > 3.5e+08] <- 8.5e+07
 
 # Try to linearise data: see linearisation.R
 
